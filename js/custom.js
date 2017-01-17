45f3b69520d1a81851d36fdc567411a7563bf3ac
siku101 <sivakumar.chandrasekar@hotmail.com>
2017-01-17 17:59:14 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,27 @@
 
  
  var lostBasketApp = angular.module('lostBasketApp', []);
+ 
+ 
  lostBasketApp.controller('lostBasketController', function ($scope, $http){
    $http.get('inputs.json').success(function(data) {
      $scope.inputs = data;
    });
+   
+   
+   $scope.selected = [];
+   
+   $scope.exist = function (selectedItem) {
+	   return $scope.selected.indexOf(selectedItem) > -1;
+   }
+   
+   
+   $scope.select = function(selectedItem) {
+	   var index = $scope.selected.indexOf(selectedItem);
+	   if ( index > -1) {
+		   $scope.selected.splice( index, 1);
+	   } else {
+		   $scope.selected.push(selectedItem);
+	   }
+   }
  });
