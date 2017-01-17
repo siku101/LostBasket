a114caf399aa92a6953a9663aa8c08d941bea5f5
siku101 <sivakumar.chandrasekar@hotmail.com>
2017-01-17 20:45:35 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,22 +6,23 @@
  lostBasketApp.controller('lostBasketController', function ($scope, $http){
    $http.get('inputs.json').success(function(data) {
      $scope.inputs = data;
-   });
    
    
    $scope.selected = [];
    
-   $scope.exist = function (selectedItem) {
-	   return $scope.selected.indexOf(selectedItem) > -1;
+   $scope.exist = function (selectedItem) {	
+   return $scope.selected.indexOf(selectedItem) > -1;
    }
    
    
-   $scope.select = function(selectedItem) {
-	   var index = $scope.selected.indexOf(selectedItem);
-	   if ( index > -1) {
-		   $scope.selected.splice( index, 1);
-	   } else {
-		   $scope.selected.push(selectedItem);
-	   }
+   $scope.select = function(selectedItem) {	
+   var index = $scope.selected.indexOf(selectedItem);	
+   if ( index > -1) {		
+   $scope.selected.splice( index, 1);	
+   } else {		
+   $scope.selected.push(selectedItem);	
    }
- });
+   }
+ });	
+ 	
+    });
